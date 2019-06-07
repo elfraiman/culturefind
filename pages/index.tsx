9d5681e950651b5c57f85daa05d3e7d8eaca0260
elfraim <elfraiman@gmail.com>
2019-06-07 18:24:01 +0200

    feat(index): working on ui and responsivness

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,29 @@ import Header from '../components/header/header';
 import css from './index.module.scss';
 
 export default class extends React.Component {
+  renderCompanyCards() {
+    const times = 15;
+    const arrayOfCard = [];
+
+    for (let i = 0; i < times; i++) {
+      arrayOfCard.push(
+        <CompanyCard
+          logo='../static/company-logos/springtree-logo.png'
+          title='Springtree'
+          subtitle={`Solving puzzles as a profession, who doesn't want that?`}
+          location='Almere, The Netherlands'
+          positions='3'
+        />,
+      );
+    }
+    return arrayOfCard;
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <div className={css.home}>
+        <div className={css.heading}>
           <div className={css.heroImage} />
 
           <div className={css.heroTitle}>
@@ -19,15 +37,16 @@ export default class extends React.Component {
           <span className={css.subTitle}>
             Find the company you'll really click with.
           </span>
+
+          <div className={css.valuePickerCard} />
+          <div className={css.body}>
+          <div className={css.companyCards}>
+          {this.renderCompanyCards()}
+          </div>
+        </div>
         </div>
 
-        <CompanyCard
-          logo='../static/company-logos/springtree-logo.png'
-          title='Springtree'
-          subtitle="Solving puzzles as a profession, who doesn't want that?"
-          location='Almere, The Netherlands'
-          positions='3'
-        />
+      
       </div>
     );
   }
