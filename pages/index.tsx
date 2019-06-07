ed563201fd60a3868de0d098354c3dd87727c26b
elfraim <elfraiman@gmail.com>
2019-06-07 19:02:11 +0200

    feat(index): added react spring animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,68 @@
 import React from 'react';
+import { animated, useSpring } from 'react-spring';
 import CompanyCard from '../components/company-card/company-card';
 import Header from '../components/header/header';
 import css from './index.module.scss';
+const Index = () => {
+  // Should make a global animations file with animations we want
+  const fadeIn = useSpring({ opacity: 1, from: { opacity: 0 } });
 
-export default class extends React.Component {
-  renderCompanyCards() {
+  const trailInLeft = useSpring({
+    opacity: 1,
+    marginLeft: 0,
+    transform: 'translate3d(0,0px,0)',
+    from: {
+      marginLeft: -1000,
+      opacity: 0,
+      transform: 'translate3d(0,-190px,0)'
+    }
+  });
+
+  const renderCompanyCards = () => {
     const times = 15;
     const arrayOfCard = [];
 
     for (let i = 0; i < times; i++) {
       arrayOfCard.push(
-        <CompanyCard
-          logo='../static/company-logos/springtree-logo.png'
-          title='Springtree'
-          subtitle={`Solving puzzles as a profession, who doesn't want that?`}
-          location='Almere, The Netherlands'
-          positions='3'
-        />,
+        <animated.div style={trailInLeft}>
+          <CompanyCard
+            logo="../static/company-logos/springtree-logo.png"
+            title="Springtree"
+            subtitle={`Solving puzzles as a profession, who doesn't want that?`}
+            location="Almere, The Netherlands"
+            positions="3"
+          />
+          ,
+        </animated.div>
       );
     }
     return arrayOfCard;
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className={css.heading}>
+  return (
+    <div>
+      <Header />
+      <div className={css.heading}>
+        <animated.div style={fadeIn}>
           <div className={css.heroImage} />
+        </animated.div>
 
-          <div className={css.heroTitle}>
-            <span className={css.culture}>Culture</span> <span>matters</span>
-            <br />
-          </div>
-
-          <span className={css.subTitle}>
-            Find the company you'll really click with.
-          </span>
-
-          <div className={css.valuePickerCard} />
-          <div className={css.body}>
-          <div className={css.companyCards}>
-          {this.renderCompanyCards()}
-          </div>
+        <div className={css.heroTitle}>
+          <span className={css.culture}>Culture</span> <span>matters</span>
+          <br />
         </div>
-        </div>
+        <span className={css.subTitle}>
+          Find the company you'll really click with.
+        </span>
 
-      
+        <div className={css.valuePickerCard} />
+
+        <div className={css.body}>
+          <div className={css.companyCards}>{renderCompanyCards()}</div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Index;
