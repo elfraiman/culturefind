285af2fc65410ab190b0a5152d4fd04f7cefae25
elfraim <elfraiman@gmail.com>
2019-12-18 17:58:51 +0100

    feat(card): spring effect

--- src/components/CompanyCard/CompanyCard.tsx
+++ src/components/CompanyCard/CompanyCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import css from "./CompanyCard.module.scss";
+import React from 'react';
+import css from './CompanyCard.module.scss';
+import { useSpring, animated } from 'react-spring';
 
 interface ICompanyProps {
   title: string;
@@ -18,15 +19,37 @@ const CompanyCard = ({
   positions,
   color
 }: ICompanyProps) => {
+  const calc: any = (x: number, y: number) => [
+    -(y - window.innerHeight / 2) / 20,
+    (x - window.innerWidth / 2) / 20,
+    1.1
+  ];
+
+  const trans: any = (x: number, y: number, s: number) =>
+    `perspective(800px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 }
+  }));
+
   return (
-    <div className={css.card}>
+    <animated.div
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans) }}
+      className={css.card}
+    >
       <div className={css.cardBody}>
-        <div className={css.img} style={{
-          backgroundImage: `url(${logo})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'contain',
-          backgroundRepeat: 'no-repeat'
-        }} />
+        <div
+          className={css.img}
+          style={{
+            backgroundImage: `url(${logo})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat'
+          }}
+        />
         <h2>{title}</h2>
         <p>{subtitle}</p>
       </div>
@@ -35,7 +58,7 @@ const CompanyCard = ({
         <span>{location}</span>
         <p> {positions} Available positions</p>
       </div>
-    </div>
+    </animated.div>
   );
 };
 
